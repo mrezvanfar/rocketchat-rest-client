38acaeddce008122f206607e07602aee63763dc9
mr <mnrezvanfar@gmail.com>
2019-09-17 18:23:28 +0800

    RocketChat Login and user registration is implemented

---
+++ src/RocketChat.php
@@ -0,0 +1,98 @@
+<?php
+namespace RocketChat;
+
+use Httpful\Request;
+
+class RocketChat extends ChatConnection{
+    private $apiURL;
+    private $adminUser="a";
+    private $adminPassword="1";
+    public $username;
+    private $password;
+    public $userId;
+    public $authToken;
+    public $nickname;
+    public $email;
+
+    public function __construct($apiURL,$username, $password, $fields = array()){
+        Request::ini(Request::init()->sendsJson()->expectsJson());
+        $this->apiURL=$apiURL;
+        $this->username = $username;
+        $this->password = $password;
+        if( isset($fields['nickname']) ) {
+            $this->nickname = $fields['nickname'];
+        }
+        if( isset($fields['email']) ) {
+            $this->email = $fields['email'];
+        }
+    }
+
+
+    public function login() {
+        $response = Request::post( $this->api . 'login' )
+            ->body(array( 'user' => $this->username, 'password' => $this->password ))
+            ->send();
+
+        if( $response->code == 200 && isset($response->body->status) && $response->body->status == 'success' ) {
+//            if( $save_auth) {
+//                // save auth token for future requests
+//                $tmp = Request::init()
+//                    ->addHeader('X-Auth-Token', $response->body->data->authToken)
+//                    ->addHeader('X-User-Id', $response->body->data->userId);
+//                Request::ini( $tmp );
+//            }
+            $this->userId = $response->body->data->userId;
+            $this->authToken = $response->body->data->authToken;
+            return true;
+        } else {
+            echo( $response->body->message . "\n" );
+            return false;
+        }
+    }
+
+
+    public function initRequestToken(){
+        Request::ini(Request::init()
+                    ->addHeader('X-Auth-Token', $this->authToken)
+                    ->addHeader('X-User-Id', $this->userId));
+    }
+
+    public function registerUser($username,$password,$email,$nickname) {
+        $response = Request::post( $this->apiURL . 'users.register' )
+            ->body(array(
+                'name' => $nickname,
+                'email' => $email,
+                'username' => $username,
+                'pass' => $password,
+            ))
+            ->send();
+        if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
+            return true;
+        } else if( strcmp($response->body->error,"Username is already in use")==0) {
+            return true;
+        }else{
+            //echo( $response->body->error . "\n" );
+            echo '<h1>Error: ';
+            print_r( $response->body->error);
+            echo '</h1>';
+            return false;
+        }
+    }
+
+
+    public function loginRegisterIfNotExist() {
+        $admin = new RocketChat($this->api, $this->adminUser, $this->adminPassword);
+        $admin->initRequestToken();
+        if( $admin->login() ) {
+            if($admin->registerUser($this->username,$this->password,$this->email,$this->nickname)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+
+}
